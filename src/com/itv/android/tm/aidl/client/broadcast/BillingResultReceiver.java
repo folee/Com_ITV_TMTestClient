662c82164e4b7530065c61e009ee35833a34b316
Folee <folee.w@gmail.com>
2013-10-12 11:00:34 +0800

    in-app billing modify

--- src/com/itv/android/tm/aidl/client/broadcast/BillingResultReceiver.java
+++ src/com/itv/android/tm/aidl/client/broadcast/BillingResultReceiver.java
@@ -3,13 +3,14 @@ package com.itv.android.tm.aidl.client.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
 public class BillingResultReceiver extends BroadcastReceiver {
-	private final String		TAG						= BillingResultReceiver.class.getSimpleName();
-	public static final String	INAPP_BILLING			= "com.itv.android.market2.billing.IN_APP_NOTIFY ";
-	public static final String	INAPP_BILLING_RECEIVED	= "com.itv.android.market2.billing.IN_APP_NOTIFY_RECEIVED ";
+	private final String		TAG				= BillingResultReceiver.class.getSimpleName();
+	public static final String	BILLINGRESULT	= "BILLINGRESULT";
+	public static final String	INAPP_BILLING	= "com.itv.android.market2.billing.IN_APP_NOTIFY";
 
 	public BillingResultReceiver() {
 		super();
@@ -18,17 +19,17 @@ public class BillingResultReceiver extends BroadcastReceiver {
 	public void onReceive(final Context context, Intent intent) {
 		String action = intent.getAction();
 		if (action.equals(INAPP_BILLING)) {
-			Log.i(TAG, "----------INAPP_BILLING SUCCESS----------");
-			Toast.makeText(context, "----------INAPP_BILLING SUCCESS----------", Toast.LENGTH_LONG).show();
-			
-			try {
-				Thread.sleep(20*1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			Log.d(TAG, "----------INAPP_BILLING Result Received----------");
+			String result = intent.getStringExtra(BILLINGRESULT);
+			if(TextUtils.isEmpty(result)){
+				Log.e(TAG, "----------INAPP_BILLING Error Result = NULL----------");
+				Toast.makeText(context, "----------INAPP_BILLING Error Result = NULL----------", Toast.LENGTH_LONG).show();
+				
+			}else{
+				Log.d(TAG, " --> BILLINGRESULT = " + result);
+				Toast.makeText(context, "----------INAPP_BILLING SUCCESS----------", Toast.LENGTH_LONG).show();
+				//TODO 这边做数据处理或传递
 			}
-			
-			Intent in = new Intent(INAPP_BILLING_RECEIVED);
-			context.sendBroadcast(in);
 		}
 	}
 }
